dd12b5ab9b7fc517631ab16fd858057f6454ed70
ChathuraSam <chathura.samarajeewa@gmail.com>
2020-06-07 19:56:31 +0530

    contact form ok now

---
+++ js/smtp.js
@@ -0,0 +1,15 @@
+function sendEmail(){
+
+	var fromEmail = document.getElementById('form_email');
+	var fromName = document.getElementById('form_name');
+	var message = document.getElementById('form_message');
+	Email.send({
+    SecureToken : "10b17316-6289-45cb-b436-ba637bd402ed",
+    To : 'chathura.samarajeewa@gmail.com',
+    From : fromEmail.value,
+    Subject : fromName.value+" has sent this from my Resume site",
+    Body : message.value
+	}).then(
+	  message => alert(message)
+	);
+}
