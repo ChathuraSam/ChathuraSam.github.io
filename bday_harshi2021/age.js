f22f1287eb66eb8108248e5b21c6fc6b0c21a801
Chathura Samarajeewa <chathura@Chathuras-MacBook-Pro.local>
2021-12-06 22:29:24 +0530

    added harshani's bday page

---
+++ bday_harshi2021/age.js
@@ -0,0 +1,45 @@
+
+
+  function calculateAge(birthYear, currentYear) {
+    var age = currentYear - birthYear;
+    console.log('You are either ' + age + ' or ' + (age - 1));
+}
+
+calculateAge(1984, 2017);
+calculateAge(1988, 2017);
+calculateAge(1982, 2017);
+
+
+
+
+
+function calculateSupply(age, numPerDay) {
+  var maxAge = 100;
+  var totalNeeded = (numPerDay * 365) * (maxAge - age);
+  var message = 'You will need ' + totalNeeded + ' cups of tea to last you until the ripe old age of ' + maxAge;
+  console.log(message);
+}
+
+calculateSupply(28, 36);
+calculateSupply(28, 2.5);
+calculateSupply(28, 400);
+
+
+function calcCircumfrence(radius) {
+  var circumference = Math.PI * 2*radius;
+  console.log("The circumference is " + circumference);
+  var area = Math.PI * radius*radius;
+  console.log("The area is " + area);
+}
+
+
+
+function ToFahrenheit(celsius) {
+  var celsiusInF = (celsius*9)/5 + 32;
+  console.log(celsius + '°C is ' + celsiusInF + '°F');
+}
+
+function fahrenheitToCelsius(fahrenheit) {
+  var fahrenheitInC = ((fahrenheit - 32)*5)/9;
+  console.log(fahrenheit + '°F is ' + fahrenheitInC + '°C');
+}
